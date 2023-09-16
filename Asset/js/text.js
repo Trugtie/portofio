bf87f7bd9ceea9e66321386a03c56bed333699b6
TrugTie <58035906+Trugtie@users.noreply.github.com>
2023-09-16 18:54:11 +0700

    Portifio

---
+++ Asset/js/text.js
@@ -0,0 +1,39 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".description .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline({ loop: false }).add({
+  targets: ".description .letter",
+  scale: [0, 1],
+  duration: 1500,
+  elasticity: 600,
+  delay: (el, i) => 150 * (i + 1),
+});
+
+const audio = document.querySelector(".audio");
+const soundPlay = document.querySelector(".soundPlay");
+const soundPause = document.querySelector(".soundMute");
+const audioBtn = document.querySelector(".audioBtn");
+
+audio.volume = 0.2;
+var isPlaying = true;
+
+soundPause.style.display = "none";
+
+audioBtn.addEventListener("click", (e) => {
+  console.log("Click " + isPlaying);
+  if (isPlaying) {
+    audio.pause();
+    soundPause.style.display = "block";
+    soundPlay.style.display = "none";
+    isPlaying = false;
+  } else {
+    audio.play();
+    soundPlay.style.display = "block";
+    soundPause.style.display = "none";
+    isPlaying = true;
+  }
+});
