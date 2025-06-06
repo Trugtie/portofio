826555b0df2539e788d646e3cd2747098ade1c7a
Won <58035906+Trugtie@users.noreply.github.com>
2025-06-06 19:33:00 +0700

    fix: fix cursor

--- Asset/js/customcursor.js
+++ Asset/js/customcursor.js
@@ -1,22 +1,18 @@
 const cursor = document.getElementById("custom-cursor");
+const body = document.body;
 
 document.addEventListener("mousemove", (e) => {
-  // Lấy kích thước của viewport
-  const viewportWidth = window.innerWidth;
-  const viewportHeight = window.innerHeight;
-
-  // Vị trí con trỏ
   let cursorX = e.pageX - cursor.offsetWidth / 2;
   let cursorY = e.pageY - cursor.offsetHeight / 2;
 
-  // Giới hạn con trỏ không ra khỏi màn hình
-  cursorX = Math.max(0, Math.min(cursorX, viewportWidth - cursor.offsetWidth));
-  cursorY = Math.max(
-    0,
-    Math.min(cursorY, viewportHeight - cursor.offsetHeight)
-  );
-
-  // Cập nhật vị trí con trỏ
   cursor.style.left = cursorX + "px";
   cursor.style.top = cursorY + "px";
+});
+
+body.addEventListener("mouseleave", (e) => {
+  cursor.style.display = "none";
+});
+
+body.addEventListener("mouseenter", (e) => {
+  cursor.style.display = "block";
 });
